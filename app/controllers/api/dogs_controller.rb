6168fb1f4d517688a2bca60face8eb56c756ebd0
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-11 21:55:09 -0500

    adding create action

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -7,6 +7,17 @@ class Api::DogsController < ApplicationController
 
   def show
     @dog = Dog.find_by(id: params[:id])
-  render 'show.json.jb'
+    render 'show.json.jb'
+  end
+
+  def create
+    @dog = Dog.new(
+      breed: params[:breed],
+      color: params[:color],
+      price: params[:price],
+      age: params[:age]
+    )
+    @dog.save
+    render 'show.json.jb'
   end
 end
